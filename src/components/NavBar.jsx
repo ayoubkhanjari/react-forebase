3b7f80c5cb247c041ed25757e7234648ea5ae1db
Ayoub Khanjari <ayoubkhanjari10@gmail.com>
2025-07-04 22:45:54 +0100

    Update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,9 @@
 import { Link, useNavigate} from "react-router-dom"; // to link to pages in react
-import "../css/NavBar.css"
+import "../css/Navbar.css"
 import { logout } from "../Config";
 import { useEffect, useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import logo from '../assets/MovieApp.png';
 
 function NavBar(){
 
@@ -30,7 +31,9 @@ function NavBar(){
     return(
         <nav className="navbar">
             <div className="navbar-brand">
-                <Link to="/home" >Movie APP</Link>
+                <Link to="/home" >
+                    <img src={logo} alt="Movie App Logo" style={{ height: '40px' }} />
+                </Link>
             </div>
             <div className="navbar-links">
                 
@@ -39,7 +42,7 @@ function NavBar(){
 
                  {/* show email only when signed in */}
                 {userEmail && (
-                <span className="nav-link user-email">
+                <span className="nav-link-user-email">
                     <strong>{userEmail}</strong>
                 </span>
                 )}
